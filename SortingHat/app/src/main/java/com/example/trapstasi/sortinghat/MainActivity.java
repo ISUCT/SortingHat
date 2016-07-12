58ad4f19e1b2c002b494b584474383edf56cb792
Silkova Anastasia <trapanastasia@gmail.com>
2016-07-12 14:01:04 +0300

    first steps

---
+++ SortingHat/app/src/main/java/com/example/trapstasi/sortinghat/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.trapstasi.sortinghat;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
