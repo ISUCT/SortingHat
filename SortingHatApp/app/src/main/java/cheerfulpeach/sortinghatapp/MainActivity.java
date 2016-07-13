0e446f16413e60773865125f04778b8af6f162dd
Silkova Anastasia <trapanastasia@gmail.com>
2016-07-14 02:28:38 +0300

    step two done

--- SortingHatApp/app/src/main/java/cheerfulpeach/sortinghatapp/MainActivity.java
+++ SortingHatApp/app/src/main/java/cheerfulpeach/sortinghatapp/MainActivity.java
@@ -1,39 +1,19 @@
 package cheerfulpeach.sortinghatapp;
 
-
-
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.view.View.OnClickListener;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    EditText FIO;
-    String result;
-
-    public final static String Extra_Message = "cheerfulpeach.sortinghatapp.MESSAGE";
-
-   @Override
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
 
-        Button resultButton = (Button) findViewById(R.id.result_button);
-        resultButton.setOnClickListener(new OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                result = FIO.getText().toString();
-                Intent intent = new Intent(MainActivity.this, result.class);
-                intent.putExtra(Extra_Message, result);
-                new result().show(getSupportFragmentManager(), "resultText");
-                startActivity(intent);
-            }
-        });
+    public void showResult(View v) {
+        new resultRes().show(getSupportFragmentManager(), "login");
     }
 }
 
--- SortingHatApp/app/src/main/java/cheerfulpeach/sortinghatapp/resultRes.java
+++ SortingHatApp/app/src/main/java/cheerfulpeach/sortinghatapp/resultRes.java
@@ -3,43 +3,29 @@ package cheerfulpeach.sortinghatapp;
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.content.DialogInterface;
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
 import android.view.View;
-import android.widget.TextView;
 
-public class result extends DialogFragment implements DialogInterface.OnClickListener {
+public class resultRes extends DialogFragment implements DialogInterface.OnClickListener {
 
     private View form=null;
-
-    TextView resultText;
-
-    String message;
-
 
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
         form = getActivity().getLayoutInflater().inflate(R.layout.result, null);
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
-        return (builder.setTitle("Отправка результата").setView(form)
-                .setPositiveButton(android.R.string.ok, this).create());
-
-        super.onCreate(savedInstanceState); {
-            Intent intent = getIntent();
-            message = intent.getStringExtra(MainActivity.Extra_Message);
-
-        }
+        return (builder.setTitle("Отправка результата").setView(form).create());
     }
 
     @Override
-    public void onClick(DialogInterface dialog, int which) {
-        resultText.setText(message);
-    }
+    public void onClick(DialogInterface dialog, int which){}
+
     @Override
     public void onDismiss(DialogInterface unused) {
         super.onDismiss(unused);
     }
+
     @Override
     public void onCancel(DialogInterface unused) {
         super.onCancel(unused);
