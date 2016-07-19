57c4547b5562637d4081e2b3441aea4cd6d40ff7
Silkova Anastasia <trapanastasia@gmail.com>
2016-07-19 11:24:30 +0300

    a little change

--- Sortinghat/app/src/main/java/cheerfulpeach/sortinghat/Player.java
+++ Sortinghat/app/src/main/java/cheerfulpeach/sortinghat/Player.java
@@ -10,9 +10,15 @@ import android.widget.Toast;
 public class Player extends AppCompatActivity {
 
     MediaPlayer mPlayer;
+    MediaPlayer mPlayer2;
+
     Button startButton;
     Button pauseButton;
     Button stopButton;
+
+    Button startButton2;
+    Button pauseButton2;
+    Button stopButton2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -53,6 +59,42 @@ public class Player extends AppCompatActivity {
                 stopAudio(view);
             }
         });
+
+
+        mPlayer2 = MediaPlayer.create(this, R.raw.mp2);
+        mPlayer2.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            @Override
+            public void onCompletion(MediaPlayer mp2) {
+                stop();
+            }
+        });
+        startButton2 = (Button) findViewById(R.id.start2);
+        pauseButton2 = (Button) findViewById(R.id.pause2);
+        stopButton2 = (Button) findViewById(R.id.stop2);
+
+        pauseButton2.setEnabled(false);
+        stopButton2.setEnabled(false);
+
+        startButton2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startAudio2(view);
+            }
+        });
+
+        pauseButton2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                pauseAudio2(view);
+            }
+        });
+
+        stopButton2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                stopAudio2(view);
+            }
+        });
     }
 
     private void stop(){
@@ -86,6 +128,27 @@ public class Player extends AppCompatActivity {
     }
 
     public void stopAudio(View view){
+
+        stop();
+    }
+
+    public void startAudio2(View view){
+
+        mPlayer2.start();
+        startButton2.setEnabled(false);
+        pauseButton2.setEnabled(true);
+        stopButton2.setEnabled(true);
+    }
+
+    public void pauseAudio2(View view){
+
+        mPlayer2.pause();
+        startButton2.setEnabled(true);
+        pauseButton2.setEnabled(false);
+        stopButton.setEnabled(true);
+    }
+
+    public void stopAudio2(View view){
 
         stop();
     }
