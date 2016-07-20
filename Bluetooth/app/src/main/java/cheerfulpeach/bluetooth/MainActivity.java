2a6b9a0fda43cb58967b3dadcf43a9a41d44abb0
Silkova Anastasia <trapanastasia@gmail.com>
2016-07-20 03:15:41 +0300

    Inteface for Bluetooth

--- Bluetooth/app/src/main/java/cheerfulpeach/bluetooth/MainActivity.java
+++ Bluetooth/app/src/main/java/cheerfulpeach/bluetooth/MainActivity.java
@@ -6,13 +6,18 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.bluetooth.*;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private final static int REQUEST_ENABLE_BT = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
 
         Button button = (Button) findViewById(R.id.button);
         button.setOnClickListener(new View.OnClickListener() {
@@ -36,6 +41,27 @@ public class MainActivity extends AppCompatActivity {
                     Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
                     startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
                 }
+
+                String status;
+                if(bluetooth.isEnabled()){
+                    String mydeviceaddress= bluetooth.getAddress();
+                    String mydevicename= bluetooth.getName();
+                    status= mydevicename+" : "+ mydeviceaddress;
+                }
+                else
+                {
+                    status="Bluetooth выключен";
+                }
+
+                Toast.makeText(MainActivity.this, status, Toast.LENGTH_LONG).show();
+            }
+        });
+
+        ImageButton quitButton = (ImageButton) findViewById(R.id.imageButton);
+        quitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
             }
         });
     }
